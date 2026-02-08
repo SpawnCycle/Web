b8c37a935be19dc0bc65e1ce4c5b0415bcfaff6e
Bagó Ádám Adrián <151737980+CsakEgyBago@users.noreply.github.com>
2026-02-08 22:46:44 +0100

    frontend: added settings page card to you guessed it, settings page

--- client/src/components/pages/Profile-Dependents/settings-page.tsx
+++ client/src/components/pages/Profile-Dependents/settings-page.tsx
@@ -1,3 +1,31 @@
+import Navbar from "@/components/navbar";
+import { WithOnloadAnimation } from "@/lib/OnloadAnimation";
+import { Card } from "@/components/ui/card";
+
 export function SettingsPage() {
-  return <div>Settings Page</div>;
+  const AnimatedNavbar = WithOnloadAnimation(Navbar);
+
+  return (
+    <div className="max-w-full w-full h-full relative flex flex-col items-center justify-start pt-10 bg-linear-to-r from-gray-700 via-black to-gray-700 text-white">
+      {
+        // Animated navbar: shows on load, then hides to -top-17
+      }
+
+      <AnimatedNavbar />
+      <Card className="z-0 flex flex-row w-350 h-150 p-10 max-w-full max-h-lg bg-gray-600 border-2 border-green-400">
+        {/* Left Section */}
+        <div className="flex-1 flex items-center justify-center flex-col gap-30">
+          <div></div>
+          <div></div>
+          <div></div>
+        </div>
+
+        {/* Middle Section */}
+        <div className="flex-1 flex items-center justify-center"></div>
+
+        {/* Right Section */}
+        <div className="flex-1 flex items-center justify-center"></div>
+      </Card>
+    </div>
+  );
 }
